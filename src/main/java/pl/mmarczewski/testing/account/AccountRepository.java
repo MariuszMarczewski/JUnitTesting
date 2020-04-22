3716f5b7ca43004bc4ab1384ee1ea90f08a8d686
Mariusz Marczewski <mariusz.marczewski1@poczta.fm>
2020-04-22 11:18:59 +0200

    initial commit

---
+++ src/main/java/pl/mmarczewski/testing/account/AccountRepository.java
@@ -0,0 +1,10 @@
+package pl.mmarczewski.testing.account;
+
+import java.util.List;
+
+public interface AccountRepository {
+
+    List<Account> getAllAccounts();
+    List<String> getByName(String name);
+
+}
---
+++ src/main/java/pl/mmarczewski/testing/order/OrderStatus.java
@@ -0,0 +1,8 @@
+package pl.mmarczewski.testing.order;
+
+public enum OrderStatus {
+
+    ORDERED, READY, DELIVERED, PREPARING, REJECTED
+
+}
+
